460cab57d0f01df64f75e59dd0fa3a7dc39435e8
Vlad Safin <djutsu17@gmail.com>
2021-02-23 20:56:35 +0200

    +

--- js/task1.js
+++ js/task1.js
@@ -7,4 +7,17 @@ const categoriesOfElements = array => {
     array.forEach((element) => console.log(`Категория:` + element.firstElementChild.textContent + `\nКоличество элементов:` + element.lastElementChild.children.length));
 }
 
+// const categoriesOfElements = array => {
+//     for (let element of array) {
+//        console.log(`Категория:` + element.firstElementChild.textContent + `\nКоличество элементов:` + element.lastElementChild.children.length);
+//    }
+// }
+
+// const categoriesOfElements = array => {
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(`Категория:` + array[i].firstElementChild.textContent + `\nКоличество элементов:` + array[i].lastElementChild.children.length)
+//     }
+// }
+
+
 categoriesOfElements(categoriesChildrenRef);
